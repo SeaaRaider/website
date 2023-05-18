73b1d7e0f11bd02c7c6031158d0da02d18ab6a5a
Mateusz Jankowski <m4.jankowski@gmail.com>
2023-05-18 17:33:22 +0200

    Weather API done

--- mainpage/weather/app.js
+++ mainpage/weather/app.js
@@ -1,31 +1,33 @@
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Rybnik, PL&appid=85608e4cff4e7f0f2b8b93c600847246";
+let url = "";
 
-window.addEventListener('load', () => {
+function weather(val){
+    url = "https://api.openweathermap.org/data/2.5/weather?q=" + val + ", PL&appid=85608e4cff4e7f0f2b8b93c600847246";
     fetch(url)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
-        //document.getElementById('city').innerHTML = data.name;
         document.getElementById('temp').innerHTML = Math.round(data.main.temp - 273) + " ºC";
         document.getElementById('tempmin').innerHTML = Math.round(data.main.temp_min - 273) + " ºC";
         document.getElementById('tempmax').innerHTML = Math.round(data.main.temp_max - 273) + " ºC";
         document.getElementById('humidity').innerHTML ="Humidity: " + data.main.humidity + " %";
         let clouds = data.weather[0].icon;
-        let ima = document.createElement('img');
 
-        ima.src =
-            clouds == '01d' || '01n' ? '/htmlstart/icons/01d.png'
-           : clouds == '02d' || '02n' ? '/htmlstart/icons/02d.png'
-           : clouds == '03d' || '03n' ? '/htmlstart/icons/03d.png'
-           : clouds == '04d' || '04n' ? '/htmlstart/icons/04d.png'
-           : clouds == '09d' || '09n' ? '/htmlstart/icons/09d.png'
-           : clouds == '10d' || '10n' ? '/htmlstart/icons/10d.png'
-           : clouds == '11d' || '11n' ? '/htmlstart/icons/11d.png'
-           : clouds == '13d' || '13n' ? '/htmlstart/icons/13d.png'
-           : clouds == '50d' || '50n' ? '/htmlstart/icons/50d.png'
-           : console.log("image error")
+        ima =
+            clouds == '01d' || '01n' ? '/mainpage/icons/01d.png'
+           : clouds == '02d' || '02n' ? '/mainpage/icons/02d.png'
+           : clouds == '03d' || '03n' ? '/mainpage/icons/03d.png'
+           : clouds == '04d' || '04n' ? '/mainpage/icons/04d.png'
+           : clouds == '09d' || '09n' ? '/mainpage/icons/09d.png'
+           : clouds == '10d' || '10n' ? '/mainpage/icons/10d.png'
+           : clouds == '11d' || '11n' ? '/mainpage/icons/11d.png'
+           : clouds == '13d' || '13n' ? '/mainpage/icons/13d.png'
+           : clouds == '50d' || '50n' ? '/mainpage/icons/50d.png'
+           : console.log("image error");
 
-        document.getElementById('clouds').appendChild(ima);
+        document.getElementById('image').src = ima;
+        document.getElementById('message').textContent = " ";
     })
-})
+    .catch(() => {
+        document.getElementById('message').textContent = "City not found";
+    })
+}
 
