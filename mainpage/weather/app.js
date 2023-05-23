82140cc9e8aa69c04c5f3119f63ddd8c3c3f25db
Mateusz Jankowski <m4.jankowski@gmail.com>
2023-05-23 17:51:29 +0200

    Weather tab done and minor changes elsewhere

--- mainpage/weather/app.js
+++ mainpage/weather/app.js
@@ -5,25 +5,13 @@ function weather(val){
     fetch(url)
     .then(response => response.json())
     .then(data => {
+        console.log(data)
         document.getElementById('temp').innerHTML = Math.round(data.main.temp - 273) + " ºC";
-        document.getElementById('tempmin').innerHTML = Math.round(data.main.temp_min - 273) + " ºC";
-        document.getElementById('tempmax').innerHTML = Math.round(data.main.temp_max - 273) + " ºC";
+        document.getElementById('feelsLike').innerHTML ="Feels like " + Math.round(data.main.feels_like - 273) + " ºC";
         document.getElementById('humidity').innerHTML ="Humidity: " + data.main.humidity + " %";
         let clouds = data.weather[0].icon;
 
-        ima =
-            clouds == '01d' || '01n' ? '/mainpage/icons/01d.png'
-           : clouds == '02d' || '02n' ? '/mainpage/icons/02d.png'
-           : clouds == '03d' || '03n' ? '/mainpage/icons/03d.png'
-           : clouds == '04d' || '04n' ? '/mainpage/icons/04d.png'
-           : clouds == '09d' || '09n' ? '/mainpage/icons/09d.png'
-           : clouds == '10d' || '10n' ? '/mainpage/icons/10d.png'
-           : clouds == '11d' || '11n' ? '/mainpage/icons/11d.png'
-           : clouds == '13d' || '13n' ? '/mainpage/icons/13d.png'
-           : clouds == '50d' || '50n' ? '/mainpage/icons/50d.png'
-           : console.error("image source error");
-
-        document.getElementById('image').src = ima;
+        document.getElementById('image').src = "/mainpage/icons/" + clouds +".png";
         document.getElementById('message').textContent = " ";
     })
     .catch(() => {
